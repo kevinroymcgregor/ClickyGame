50214341ffcbf1839c2c767bcc1e4c1e27af7177
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-09-30 14:30:02 -0700

    navBar is rendering with the score and high score being passed from app

--- clicky_game/src/components/navbar.js
+++ clicky_game/src/components/navbar.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+class navbar extends React.Component {
+    render() {
+        const { score, highScore } = this.props;
 
+        return (
+            <div>
+                {score} | {highScore}
+            </div>
+        )
+    }
+}
 
 export default navbar;
