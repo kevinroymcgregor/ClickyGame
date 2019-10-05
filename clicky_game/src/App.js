535f8e885e1d4cf20910744d7b75cf9482771706
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-10-05 07:56:49 -0700

    turning in

--- clicky_game/src/App.js
+++ clicky_game/src/App.js
@@ -2,20 +2,37 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/navbar'
 import Header from './components/header'
-import Body from './components/body'
+import Picture from './components/picture';
 
 class App extends React.Component {
   state = {
     score: 0,
     highScore: 0
   }
-  
+
+  addScore = () => {
+		this.setState({
+			score: (this.state.score + 1)
+		});
+	}
+
   render() {
     return (
       <div className="App">
-        <Navbar score= {this.state.score} highScore= {this.state.highScore} />
+        <Navbar score={this.state.score} highScore={this.state.highScore} />
         <Header />
-        <Body />
+        <Picture image="x-wing" onClick={this.addScore} />
+        <Picture image="tie-fighter" onClick={this.addScore} />
+        <Picture image="millenium-falcon" onClick={this.addScore} />
+        <Picture image="death-star-2" onClick={this.addScore} />
+        <Picture image="a-wing" onClick={this.addScore} />
+        <Picture image="hyena-bomber" onClick={this.addScore} />
+        <Picture image="land-speeder" onClick={this.addScore} />
+        <Picture image="slave-one" onClick={this.addScore} />
+        <Picture image="star-destroyer" onClick={this.addScore} />
+        <Picture image="tie-advanced" onClick={this.addScore} />
+        <Picture image="tie-defender" onClick={this.addScore} />
+        <Picture image="tie-interceptor" onClick={this.addScore} />
       </div>
     );
   }
--- clicky_game/src/components/picture.js
+++ clicky_game/src/components/picture.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 class Picture extends React.Component {
-
+    
+    
     render() {
+        const onClick = this.props.onClick
         return (
             <div>
-                <img src={`/imgs/${this.props.image}.jpg`} alt={this.props.image}></img>
+                <img src={`/imgs/${this.props.image}.jpg`} alt={this.props.image} onClick={() => onClick()} />
             </div>
         );
     }
